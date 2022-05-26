973a86cb0cee48bf4bab397365275f0dd7097777
Mustapha <mzeamari@bol.com>
2022-05-26 19:58:41 +0200

    fix path to artifacts and db file and finish last question

--- modules/module_2/launch_mlflow_server.sh
+++ modules/module_2/launch_mlflow_server.sh
@@ -1,4 +1,4 @@
 mlflow server \
---backend-store-uri sqlite:///backend.db \
---default-artifact-root ./artifacts \
+--backend-store-uri sqlite:////Users/mzeamari/repos/data-science/mlops-zoomcamp-2022/modules/module_2/backend.db \
+--default-artifact-root /Users/mzeamari/repos/data-science/mlops-zoomcamp-2022/modules/module_2/artifacts \
 --host 0.0.0.0:5001
--- modules/module_2/module_2_homework.py
+++ modules/module_2/module_2_homework.py
@@ -6,17 +6,29 @@ from mlflow.entities import ViewType
 from mlflow.tracking import MlflowClient
 
 MLFLOW_TRACKING_URI = "http://localhost:5001"
-
+HPO_EXPERIMENT_NAME = "random-forest-hyperopt"
+BEST_MODELEXPERIMENT_NAME = "random-forest-best-models"
 ANSWERS = {}
 
 mlflow_client = MlflowClient(MLFLOW_TRACKING_URI)
-
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 
 def print_answers():
     for k, v in ANSWERS.items():
         print(f'{k} \n\t{v}')
+
+
+def get_best_run_experiment(experiment_name):
+    global best_run_hpo_experiment
+    hpo_experiment = mlflow_client.get_experiment_by_name(experiment_name)
+    best_run_hpo_experiment = mlflow_client.search_runs(
+        experiment_ids=hpo_experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.rmse ASC"]
+    )
+    return best_run_hpo_experiment[0]
 
 
 if __name__ == '__main__':
@@ -36,18 +48,10 @@ if __name__ == '__main__':
     ANSWERS["Q4: In addition to backend-store-uri, what else do you need to pass to properly configure the server?"] = \
         "default-artifact-root"
 
-    hpo_experiment = mlflow_client.get_experiment_by_name("random-forest-hyperopt")
-    best_run_hpo_experiment = mlflow_client.search_runs(
-        experiment_ids=hpo_experiment.experiment_id,
-        run_view_type=ViewType.ACTIVE_ONLY,
-        max_results=1,
-        order_by=["metrics.rmse ASC"]
-    )
+    ANSWERS["Q5: What's the best validation RMSE that you got?"] = \
+        get_best_run_experiment(HPO_EXPERIMENT_NAME).data.metrics['rmse']
 
-    ANSWERS["Q5: What's the best validation RMSE that you got?"] = best_run_hpo_experiment[0].data.metrics['rmse']
-
-
-    best_model_name = "sk-learn-random-forest-reg"
-    ANSWERS["Q6: What is the test RMSE of the best model?"] = ""
+    ANSWERS["Q6: What is the test RMSE of the best model?"] = \
+        get_best_run_experiment(BEST_MODELEXPERIMENT_NAME).data.metrics['test_rmse']
 
     print_answers()
