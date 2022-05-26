bc173f274b30e6ec64d7d38118e6a8e973c43b7e
Mustapha <mzeamari@bol.com>
2022-05-26 15:18:50 +0200

    clean up code of module 1

--- modules/module_1/module_1_homework.py
+++ modules/module_1/module_1_homework.py
@@ -10,61 +10,45 @@ VALIDATION_SET = 'fhv_tripdata_2021-02.parquet'
 DATA_DIR = './data/'
 FEATURES = ['PUlocationID', 'DOlocationID']
 TARGET = 'duration'
+ANSWERS = {}
 
 
-def _load(filename: str) -> DataFrame:
-    return pd.read_parquet(DATA_DIR + filename)
-
-
-def _preprocess(df: DataFrame) -> (dict, pd.Series):
-    _df = df.copy()
-    _df = _add_duration_column(_df)
-    _df = _impute_missing_values(_df)
-    _df_filtered = _filter_duration_between_1_and_60_min(_df)
-    transformed_features = _df_filtered[FEATURES].to_dict(orient='records')
-    target = _df_filtered[TARGET].values
-    return transformed_features, target
-
-
-def _filter_duration_between_1_and_60_min(_df):
-    return _df[(_df['duration'] >= 1) & (_df['duration'] <= 60)]
-
-
-def _impute_missing_values(_df: DataFrame) -> DataFrame:
-    _df['PUlocationID'] = _df['PUlocationID'].fillna(-1)
-    _df['DOlocationID'] = _df['DOlocationID'].fillna(-1)
-    return _df
-
-
-def _add_duration_column(_df: DataFrame) -> DataFrame:
+def _load_data(filename: str) -> DataFrame:
+    _df = pd.read_parquet(DATA_DIR + filename)
+    ANSWERS["Q1: Read the data for January. How many records are there?"] = len(_df)
     _df['duration'] = _df['dropOff_datetime'] - _df['pickup_datetime']
-    _df['duration'] = _df['duration'].apply(lambda td: td.total_seconds() / 60)
+    _df['duration'] = _df['duration'].dt.total_seconds() / 60
+    ANSWERS["Q2: What's the average trip duration in January?"] = _df['duration'].mean()
+    _df = _df[(_df['duration'] >= 1) & (_df['duration'] <= 60)].copy()
+    ANSWERS["Q3: What's the fractions of missing values for the pickup location ID? "] = \
+        _df['PUlocationID'].isnull().mean()
+    _df[FEATURES] = _df[FEATURES].fillna(-1).astype('int').astype('str')
     return _df
 
 
-def train() -> Pipeline:
-    training_data = _load(TRAIN_SET)
-    transformed_features, target = _preprocess(training_data)
-    trainer = ModelTrainer(features=transformed_features, target=target)
-    pipeline = trainer.train_model()
-    return pipeline
+def train(_df: DataFrame) -> Pipeline:
+    feature_dicts = _df[FEATURES].to_dict(orient='records')
+    target = _df[TARGET].values
+    trainer = ModelTrainer(features=feature_dicts, target=target)
+    return trainer.train_model()
 
 
-def validate(model: Pipeline, X: dict, y: pd.Series):
+def validate(model: Pipeline, _df: DataFrame) -> float:
+    X = _df[FEATURES].to_dict(orient='records')
+    y = _df[TARGET].values
     y_pred = model.predict(X)
     return mean_squared_error(y, y_pred, squared=False)
 
 
 if __name__ == '__main__':
-    model = train()
-    train_data = _load(TRAIN_SET)
-    validation_data = _load(VALIDATION_SET)
+    train_data = _load_data(TRAIN_SET)
+    validation_data = _load_data(VALIDATION_SET)
 
-    X_train, y_train = _preprocess(train_data)
-    X_validate, y_validate = _preprocess(validation_data)
+    model = train(train_data)
 
-    rmse_train = validate(model, X_train, y_train)
-    rmse_validate = validate(model, X_validate, y_validate)
+    ANSWERS["Q4: What's the dimensionality of this matrix?"] = len(model['vect'].feature_names_)
+    ANSWERS["Q5: What's the RMSE on train?"] = validate(model, train_data)
+    ANSWERS["Q5: What's the RMSE on validation?"] = validate(model, validation_data)
 
-    print(f"RMSE on train: {rmse_train}")
-    print(f"RMSE on validate: {rmse_validate}")
+    for k, v in ANSWERS.items():
+        print(f'{k} \n\t{v}')
